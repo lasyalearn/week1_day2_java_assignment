a760032e17f18e9161e4fd7b478d981106a7f9c9
lasyalearn <lasyas.gitrepo@gmail.com>
2025-01-09 10:28:47 +0530

    week1 day2 new third commit andded  comments

--- src/day2_new/q4/EmployeeTest.java
+++ src/day2_new/q4/EmployeeTest.java
@@ -4,6 +4,7 @@ public class EmployeeTest
 {
     public static void main(String[] args)
     {
+        // we can't create a class of an abstract class
         ArrayList<Employee> employees = new ArrayList<>();
         employees.add(new SalariedEmployee("Koerber1", 1,2000));
         employees.add(new HourlyEmployee("Koerber2", 2, 40,10));
